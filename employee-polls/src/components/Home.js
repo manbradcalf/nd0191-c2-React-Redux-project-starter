0ac75d84aeb315ee96ef033000b22d0252e556fc
Ben Medcalf <ben.medcalf@gmail.com>
2023-11-04 15:03:10 -0400

    little bit of cleanup

--- employee-polls/src/components/Home.js
+++ employee-polls/src/components/Home.js
@@ -1,21 +1,33 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-const Home = ({ questions, employes }) => {
-  return (
-    <div className="bordered">
-      <h1>Home component!</h1>
-      <h2>Question of the day</h2>
-      <p>{questions && JSON.stringify(questions)}</p>
-      <b>OR</b>
-      <h2>Asked by</h2>
-    </div>
-  );
+const Home = ({ questionsProp, employeesProp, loadingProp }) => {
+  if (!loadingProp) {
+    return (
+      <div className="bordered">
+        <h1>Home component!</h1>
+        <h2>Question of the day</h2>
+        <p>{questionsProp[0].optionOne.text}</p>
+        <b>OR</b>
+        <p>{questionsProp[0].optionTwo.text}</p>
+        <h2>Asked by</h2>
+        <p>{questionsProp[0].author}</p>
+      </div>
+    );
+  } else {
+    return <h1>Loading!</h1>;
+  }
 };
 
-const mapStateToProps = ({ questions, employees }) => ({
-  questions,
-  employees,
-});
+const mapStateToProps = ({ questions, employees }) => {
+  // map the data to a list. the ids are actally in the values already
+  const questionsList = questions ? Object.values(questions) : [];
+  const employeesList = employees ? Object.values(employees) : [];
+  return {
+    questionsProp: questionsList,
+    employeesProp: employeesList,
+    loadingProp: questionsList.length === 0 && employeesList.length === 0,
+  };
+};
 
 export default connect(mapStateToProps)(Home);
