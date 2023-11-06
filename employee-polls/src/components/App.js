c83a4c758063451d24c6a231cf8124d28981285f
Ben Medcalf <ben.medcalf@gmail.com>
2023-11-05 21:41:07 -0500

    navbar into own component

--- employee-polls/src/components/App.js
+++ employee-polls/src/components/App.js
@@ -4,30 +4,20 @@ import { useEffect } from 'react';
 import { handleInitialData } from '../actions/shared';
 import { connect } from 'react-redux';
 import Home from './Home';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Questions from './Questions';
 import Leaderboard from './Leaderboard';
+import { NavBar } from './NavBar';
 
 const App = ({ dispatch, employees, questions }) => {
   useEffect(() => {
     dispatch(handleInitialData());
   }, []);
-  console.log('employees', employees);
-  console.log('questions', questions);
 
   return (
     <div className="app">
       <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/questions">Questions</Link>
-            </li>
-          </ul>
-        </nav>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/questions" element={<Questions />} />
---
+++ employee-polls/src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export const NavBar = () => {
+  return (
+    <div className='bordered'>
+    <nav className='navbar'>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/questions">Questions</Link>
+        </li>
+        <li>
+          <Link to="/leaderboard">Leaderboard</Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  );
+};
