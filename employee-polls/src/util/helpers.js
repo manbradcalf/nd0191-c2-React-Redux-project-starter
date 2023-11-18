c760eb649c249f34fd3d630ca308b775deff230c
Ben Medcalf <ben.medcalf@gmail.com>
2023-11-18 12:49:43 -0500

    styling

--- employee-polls/src/util/helpers.js
+++ employee-polls/src/util/helpers.js
@@ -5,7 +5,7 @@ export const authedComponent = (authedUser, component, componentName) => {
   if (!authedUser) {
     return (
       <Paper sx={{ p: 2 }}>
-        <Typography variant="h4" sx={{ p: 2, m: 2 }}>
+        <Typography variant="h4" sx={{ p: 2, m: 2, textAlign:"center" }}>
           Please login to view {componentName}
         </Typography>
         <Login />
