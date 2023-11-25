31d8b1907433d0060887dde4dcd38c93c3121690
Ben Medcalf <ben.medcalf@gmail.com>
2023-11-25 15:31:01 -0500

    fix warning in app.js

--- employee-polls/src/index.js
+++ employee-polls/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import middleware from './middleware';
+import { createRoot } from 'react-dom/client';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -13,11 +13,12 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 const store = createStore(reducer, middleware);
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   // give Provider the store so they can stick it on context
   // everything inside Provider has access to store now
   <Provider store={store}>
     <App dispatch={store.dispatch} />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
